a98447114cce9b26ac2ccebc51f95baa34801266
Janco Tanis <git@jancology.com>
2024-02-14 16:17:56 +0100

    doc error

--- lib/wrapi/respond_to.rb
+++ lib/wrapi/respond_to.rb
@@ -2,13 +2,13 @@
 module WrAPI
   module RespondTo
 
-    # Delegate to Integra365::Client
+    # Delegate to Client
     def self.method_missing(method, *args, &block)
       return super unless client.respond_to?(method)
       client.send(method, *args, &block)
     end
 
-    # Delegate to Integra365::Client
+    # Delegate to Client
     def self.respond_to?(method, include_all = false)
       client.respond_to?(method, include_all) || super
     end
